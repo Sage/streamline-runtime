3fb271594e2a04e2af36875244737035b26ffcdb
Bruno Jouhier <bjouhier@gmail.com>
2015-10-01 22:16:19 +0200

    moved flows to streamline-runtime

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 "use strict";
-// use generators as default browser runtime for now 
-// it works with generators (or regenerator)
-module.exports = require('./lib/runtime-generators');
+module.exports = {
+	flows: require('./lib/flows'),
+}
