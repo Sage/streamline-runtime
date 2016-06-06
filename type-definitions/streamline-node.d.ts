c98eefbf0c9fbd78b7e72eb04b9c724f8b68ce9b
Bruno Jouhier <bjouhier@gmail.com>
2016-06-07 01:35:33 +0200

    attempt 3 on .d.ts files

--- type-definitions/streamline-node.d.ts
+++ type-definitions/streamline-node.d.ts
@@ -1,12 +1,7 @@
 
-import StreamlineRuntime from "streamline-runtime";
-
-declare interface Array<T> {
-    forEach_: (_: StreamlineRuntime, fn: (_: StreamlineRuntime, elt: T, index: number) => void) => void;
-    map_: (_: StreamlineRuntime, fn: (_: StreamlineRuntime, elt: T, index: number) => T) => Array<T>;
-}
-
+// just an embryo - for testing
 declare module "fs" {
+    import StreamlineRuntime from "streamline-runtime";
     export function readFile(path: string, encoding: string, _: StreamlineRuntime) : string;
     export function readFile(path: string, _: StreamlineRuntime) : Buffer;
     export function writeFile(path: string, data: Buffer, _: StreamlineRuntime) : void;
